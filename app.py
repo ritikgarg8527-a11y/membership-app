319c335825ded569e57674304206400b4b24cf6c
ritikgarg8527-a11y <ritikgarg.8527@gmail.com>
2026-04-24 14:50:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -100,22 +100,6 @@ if menu == "Dashboard":
 
 # ---------- ADD ----------
 elif menu == "Add":
-    add_access = False   # ❌ Change to True when allowed
-    if not add_access:
-        st.markdown("""
-<div style="
-    background: linear-gradient(135deg, #6b8e23, #556b2f);
-    padding: 18px;
-    border-radius: 10px;
-    text-align: center;
-    font-size: 24px;
-    font-weight: bold;
-    color: white;
-    box-shadow: 0 4px 10px rgba(0,0,0,0.3);
-">
-🚫 Access Restricted: Please take permission from Management
-</div>
-""", unsafe_allow_html=True)
     st.subheader("➕ Add Member")
 
     member_type = st.selectbox("Select Member Type", ["Primary", "Family"])
@@ -153,7 +137,7 @@ elif menu == "Add":
         location = st.text_input("Location *")
         remarks = st.text_input("Remarks")
 
-    if st.button("Add Member", disabled=not add_access):
+    if st.button("Add Member"):
 
         if member_type == "Primary":
             if not (id_ and user_id and membership and fname and sname and phone1 and location and email):
@@ -258,24 +242,6 @@ elif menu == "Search/Edit":
 
                     if st.button(f"💾 Save {i}"):
 
-                        idx = int(row.name) + 2
-                        membership_no = str(row["MemberShip No"])
-
-                        # ✅ UPDATE ONLY CURRENT LOCATION
-                        sheet.update(f"Q{idx}", [[clean(location)]])
-                        # ✅ IF PRIMARY → UPDATE FAMILY LOCATION
-                        if row["Type"] == "Primary":
-
-                            all_data = sheet.get_all_records()
-
-                            for j, r in enumerate(all_data):
-                                if str(r["MemberShip No"]) == membership_no and r["Type"] == "Family":
-
-                                    family_row_index = j + 2
-                                    sheet.update(f"Q{family_row_index}", [[clean(location)]])
-                        del st.session_state.edit_index
-                        st.success("Address Updated for Primary + Family")
-                        st.rerun()
                         idx = int(row.name) + 2
 
                         row_data = [
